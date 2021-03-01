1391799c348f72b3b6bb0cd8ef48168b3c6af000
choisangsoo <css1109@gmail.com>
2021-03-01 23:12:19 +0900

    리엑트 테스트

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,14 +1,28 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  let a = 'cc';
+function App(){
+
+  let [글제목, 글제목변경] = useState(['남자 코트', '강남 우동', '세번째제목']); 
+  //변수 대신 사용 가능 공간
+  //자주 바뀌거나 중요한 데이터는 useState를 써라 필수~!!!
+  //변수처럼 새로고침 안해도 재랜더링 됨
+  
+  let posts = '강남 고기 맛집';
   return (
     <div className="App">
-      {a}
-      
+      <div className="black-nav">
+        <div>개발 blog</div>
+      </div>
+     
+      <div className="list">
+          <h3>{ 글제목[0] }</h3>
+          <p>2월 17일 발행</p>
+          <hr/>
+        </div>
     </div>
-  );
+  )
 }
 
 export default App;
