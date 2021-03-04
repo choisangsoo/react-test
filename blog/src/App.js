f3b375bd79db9a668a9565d428f6de7f945e512e
choisangsoo <css1109@gmail.com>
2021-03-04 23:22:45 +0900

    state 사용법

--- blog/src/App.js
+++ blog/src/App.js
@@ -13,13 +13,20 @@ function App(){
   //자주 바뀌거나 중요한 데이터는 useState를 써라 필수~!!!
   //변수처럼 새로고침 안해도 재랜더링 됨
   
+  function 제목바꾸기()
+  {
+    var newArray = [...글제목];
+    newArray[0] = '여자 코드';
+    글제목변경(newArray);
+  }
+
   let posts = '강남 고기 맛집';
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 blog</div>
       </div>
-     
+      <button onClick={ 제목바꾸기 }>버튼</button>
       <div className="list">
           <h3>{ 글제목[0] }  <span onClick={ ()=>{ 따봉변경(따봉+1)} }>👍</span> { 따봉 } </h3>
           <p>2월 17일 발행</p>
